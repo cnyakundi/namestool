eabd15956a22be905bbb3fc6d213149668bead08
Cyprian  Nyakundi <cn@Cyprians-MacBook-Pro.local>
2020-08-24 18:54:33 +0300

    add js backend logic

---
+++ javascript/script.js
@@ -0,0 +1,19 @@
+// Business (or back-end) logic:
+var akan = function (y, m, d, g) {
+  var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+  var maleNames = [
+    "Kwasi",
+    "Kudwo",
+    "Kwabena",
+    "Kwaku",
+    "Yaw",
+    "Kofi",
+    "Kwame",
+  ];
+  var d = new Date(y, --m, d);
+  if (g === "Female") {
+    return d && femaleNames[d.getDay()];
+  } else {
+    return d && maleNames[d.getDay()];
+  }
+};
